2f98f10d302ebc902cb6c8cc5a3c7a654a213e29
Ahmed-prog70 <rashdiahmed03@outlook.com>
2024-11-11 22:25:43 +0500

    Create Q6.c

---
+++ Lab_Task09/Q6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void find_max_min(int arr[], int n, int *max, int *min)
+{
+    *max = arr[0];
+    *min = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > *max)
+            *max = arr[i];
+        if (arr[i] < *min)
+            *min = arr[i];
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter number of elements in array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    int max, min;
+    find_max_min(arr, n, &max, &min);
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d\n", min);
+    return 0;
+}
