7eb3890919f7c7b94d69a57afcbf6000ccecedb9
Ahmed-prog70 <rashdiahmed03@outlook.com>
2024-11-25 19:26:38 +0500

    Create Q5.c

---
+++ Lab_Task10/Q5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void bubbleSort(int arr[], int n)
+{
+    if (n == 1)
+        return;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+    bubbleSort(arr, n - 1);
+}
+int main()
+{
+    int n, arr[999];
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, n);
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
