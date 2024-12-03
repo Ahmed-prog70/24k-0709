97d5a269b6d30512b2d66a0573afa50531786bc1
Ahmed-prog70 <rashdiahmed03@outlook.com>
2024-12-03 17:45:29 +0500

    Update Q1.C

--- Lab_Task11/Q1.C
+++ Lab_Task11/Q1.C
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+int main()
+{
+    FILE *file = fopen("dummy.txt", "r");
+    char content[50];
+    if (file == NULL)
+    {
+        printf("Error");
+        return 1;
+    }
+    fscanf(file, "%s", content);
+    printf("Content: %s\n", content);
+    fclose(file);
+    file = fopen("data.txt", "w");
+    if (file == NULL)
+    {
+        printf("Error");
+        return 1;
+    }
+    fprintf(file, "%s\n", content);
+    fclose(file);
+}
